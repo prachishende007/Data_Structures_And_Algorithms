e53a4355f1a88854beddeff09a3bdc4eecbc53e5
Prachi Shende <prachishende182@gmail.com>
2025-08-13 00:25:31 +0530

    Add files via upload

---
+++ learnc++.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//Functions
+// int sum(int a){
+    
+
+//     if(a == 0 || a == 1){
+//         return (1);
+//     }
+//     else{
+//         int fact = a * sum(a-1);
+//     }
+// }
+
+int main()
+{
+    int fact;
+    // int x;
+    // cin>>x;
+    // cout<<"the valuse of x"<<x;
+
+    // string which ony takes a word
+    // string s;
+    // cin>>s;
+    // string s ="Prachi";
+    // int len = s.size();
+
+    // s[len -1] = 'e';
+
+    //string for entire line getline
+    // string str;
+    // getline(cin,str);
+    // cout<<str;
+
+
+    //switch
+    // int day;
+    // cin>>day;
+    // switch (day)
+    // {
+    // case 1:
+    //     cout<<"Monday";
+    //     break;
+    
+    // default:
+    //     break;
+    // }
+
+    // Array
+    // int arr[5];
+    // cin>>arr[0]>>arr[1];
+
+    // arr[1] +=10;
+    // cout<<arr[1];
+
+    //2D Array
+    // int arr[3][5];
+     
+
+    //Function
+    // int a;
+    // cin>>a;
+
+    // fact = sum(a);
+
+    // cout<<"The factorial is "<<fact;
+
+    return(0);
+
+}
