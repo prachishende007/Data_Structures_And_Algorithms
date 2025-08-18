bd26d20dafd6dab678e77932341a20e836d84a2b
Prachi Shende <prachishende182@gmail.com>
2025-08-18 12:07:33 +0530

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    vector<int> v(5,100);
+
+    //singly linked list is used to store the vector
+    
+    //iterators
+    // vector<int>::iterator it = v.begin();
+    // it++;
+    // cout<<*(it)<<" ";
+
+    //last element
+    // cout<<v.back()<<" ";
+
+    // //iteration of vector
+    // for(vector<int>::iterator it = v.begin(); it != v.end(); it++){
+
+    //     cout<<*(it)<<" ";
+    // }
+
+    // //auto of vector
+    // for(auto it = v.begin(); it != v.end(); it++){
+
+    //     cout<<*(it)<<" ";
+    // }
+
+    // //for each loop
+    // for(auto it : v){
+
+    //     cout<<it<<" ";
+    // }
+
+
+    //Erase
+    // v.erase(v.begin());
+    // v.erase(v.begin()+2,v.begin()+3);
+
+    //Insert
+    // v.insert(v.begin(),300);
+    // v.insert(v.begin(), 2, 10); //2 occurences of 10
+
+    // vector<int> copy(2,50);
+    // // v.insert(v.begin(), copy.begin(), copy.end()); // inserting another vector in it
+
+    // v.size();
+    // v.pop_back(); // last element
+    // v.swap(copy);
+    // v.clear();
+    // v.empty();
+
+    
+    // // next_permutation(v.begin(), v.end());
+    // if want all permutation it should be started
+
+
+}
