69e926a6fdaaa8709c94cb1b4433a49e50e5a4b2
Prachi Shende <prachishende182@gmail.com>
2025-08-24 13:59:08 +0530

    Add files via upload

---
+++ Deque.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    deque<int>dq;
+    dq.push_back(1);
+    dq.emplace_back(3);
+
+    // rest operations are same as vectore
+    // begin, end, rbegin, rend, clear, insert, size, swap
+    
+}
